561f3120c137a01a70c0919946484b2312a1b888
Adnan Siddiqui <60623435+adnansid99@users.noreply.github.com>
2022-07-07 00:16:27 +0600

    deploy "Sid-Tor-Api"

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-python3 update.py && python3 -m publicleechgroup
+python3 update.py && python3 main.py
--- update.py
+++ update.py
@@ -4,23 +4,23 @@ from requests import get as rget
 from dotenv import load_dotenv
 from sys import executable
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            pass
-    except:
-        pass
-except:
-    pass
+# CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
+# try:
+#     if len(CONFIG_FILE_URL) == 0:
+#         raise TypeError
+#     try:
+#         res = rget(CONFIG_FILE_URL)
+#         if res.status_code == 200:
+#             with open('config.env', 'wb+') as f:
+#                 f.write(res.content)
+#         else:
+#             pass
+#     except:
+#         pass
+# except:
+#     pass
 
-load_dotenv('config.env', override=True)
+# load_dotenv('config.env', override=True)
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
@@ -28,19 +28,19 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/adnansidd99/PublicLeech"
+    UPSTREAM_REPO = "https://github.com/adnansid99/Sid-Tor-Api"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
 except:
-    UPSTREAM_BRANCH = 'master'
+    UPSTREAM_BRANCH = 'main'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
                  && git config --global user.email asr3012003@gmail.com \
-                 && git config --global user.name adnansidd99 \
+                 && git config --global user.name adnansid99 \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \
