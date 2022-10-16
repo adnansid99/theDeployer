7cef6572e62c7100bfbef45e17a921840e78a920
Adnan Siddiqui <60623435+adnansid99@users.noreply.github.com>
2022-10-16 23:44:21 +0600

    Update update.py

--- update.py
+++ update.py
@@ -1,26 +1,7 @@
 from os import path as ospath, environ, execl as osexecl
 from subprocess import run as srun
 from requests import get as rget
-from dotenv import load_dotenv
 from sys import executable
-
-# CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-# try:
-#     if len(CONFIG_FILE_URL) == 0:
-#         raise TypeError
-#     try:
-#         res = rget(CONFIG_FILE_URL)
-#         if res.status_code == 200:
-#             with open('config.env', 'wb+') as f:
-#                 f.write(res.content)
-#         else:
-#             pass
-#     except:
-#         pass
-# except:
-#     pass
-
-# load_dotenv('config.env', override=True)
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
@@ -28,7 +9,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/adnansid99/Sid-Tor-Api"
+    UPSTREAM_REPO = "https://github.com/l3v11/SearchX"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
